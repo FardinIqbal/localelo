d401f7ce215a45d71e636e4210ebffda352bd434
Fardin Iqbal <52869603+FardinIqbal@users.noreply.github.com>
2025-11-14 22:40:10 -0500

    Update organization overview stats and show match rating changes

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,2 +1,20 @@
 module OrganizationsHelper
+  def rating_change_badge(participant)
+    return unless participant&.elo_change
+
+    change = participant.elo_change.round
+    classes = if change.positive?
+                "bg-green-400/20 text-green-400"
+              elsif change.negative?
+                "bg-red-400/20 text-red-400"
+              else
+                "bg-yellow-400/20 text-yellow-400"
+              end
+
+    content_tag(
+      :span,
+      "#{change.positive? ? '+' : ''}#{change} ELO",
+      class: "ml-2 px-2 py-0.5 rounded-full text-xs font-medium #{classes}"
+    )
+  end
 end
