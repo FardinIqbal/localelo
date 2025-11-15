f5b8e112eb30845ec93218af1c90c01691ab4a2e
Fardin Iqbal <52869603+FardinIqbal@users.noreply.github.com>
2025-11-14 22:19:55 -0500

    Process ratings when matches are logged

--- app/jobs/rating_calculation_job.rb
+++ app/jobs/rating_calculation_job.rb
@@ -1,25 +1,15 @@
-# app/jobs/rating_calculation_job.rb
-
 class RatingCalculationJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # This job will be triggered periodically (e.g., daily)
-    # It will process all matches since the last rating period
-
-    # For each leaderboard, calculate new ratings
+  def perform(*_args)
     Leaderboard.find_each do |leaderboard|
-      # Get all matches that have not been rated yet for this leaderboard
-      # This assumes a 'rated_at' timestamp on the Match model, which we'll need to add.
-      # For now, we'll just get all matches.
-      matches = leaderboard.matches.where(rated_at: nil)
-
-      next if matches.empty?
-
-      RatingService.calculate_ratings(leaderboard, matches)
-
-      # Mark matches as rated
-      matches.update_all(rated_at: Time.current)
+      leaderboard.matches
+                 .active
+                 .where(rated_at: nil)
+                 .includes(match_participants: :profile)
+                 .find_each do |match|
+        RatingService.process_match(match)
+      end
     end
   end
 end
